f21aeed81f5d920f3e008558949001cbc80646a9
Jemberry <2425770361@qq.com>
2026-06-03 09:06:55 +0800

    Add:day21_files

---
+++ day21/03_order_containers.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <forward_list>
+
+using namespace std;
+
+/*
+容器分类:
+1.顺序容器:
+    array : 静态数组
+    vector : 动态数组
+    forward_list: 单链表
+    list : 双向链表
+    deque: 双向队列
+
+2.关联容器: 底层实现是红黑树
+    set
+    map
+    multimap
+    multiset
+
+3.无序关联式容器: 底层实现是哈希表
+    unordered_set
+    unordered_map
+    unordered_multimap
+    unordered_multiset
+*/
+
+// 了解 array, forward_list
+void test1()
+{
+    array<int,5> arr {1,2,3,4,5};
+    cout << arr[0] << endl;
+    cout << arr.at(1) << endl;
+    cout << arr.front() << endl;
+    cout << arr.back() << endl;
+    cout << "--------" << endl;
+
+    forward_list<int> fl {4, 5, 6};
+    for(auto & e : fl)
+    {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    test1();
+    return 0;
+}
