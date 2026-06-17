0239de085001947c9e4a0e42aa170486641d051f
Jemberry <2425770361@qq.com>
2026-06-17 09:07:30 +0800

    Add:Day05_files

---
+++ cpp_project/Day05/02_assert.cc
@@ -0,0 +1,20 @@
+// 运行时断言:
+//   判断表达式是否为真
+//      真：程序继续执行
+//      假：异常终止
+// 静态断言:
+//
+#include <assert.h>
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    // Q: 如何给定出错信息（文本）?
+    // assert(a == 5 && "a不等于5");
+
+    // 编译时判断表达式的值
+    static_assert(sizeof(int) == 4, "int的大小不为4");
+
+    printf("main end\n");
+}
